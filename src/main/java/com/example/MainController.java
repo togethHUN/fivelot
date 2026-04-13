84ed3c1ed2796e2147bc09c87cfe10e7c31152ca
Tóth Gergely <valami@com>
2026-04-13 13:20:32 +0200

    Kezdés!

---
+++ src/main/java/com/example/MainController.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+public class MainController {
+
+    @FXML
+    private GridPane gridPane;
+
+    @FXML
+    void initialize() {};
+
+    @FXML
+    private Button onClickSaveButton;
+
+}
